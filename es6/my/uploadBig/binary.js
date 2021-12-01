a1318dac472a8409eed94ae057134bcca5886839
chuang.wang <chuang.wang@ubtrobot.com>
2021-12-01 19:28:20 +0800

    feat(binary): 二进制

---
+++ es6/my/uploadBig/binary.js
@@ -0,0 +1,14 @@
+/**
+ * 玩转前端二进制
+ * https://mp.weixin.qq.com/s/QHi6BVM5Jt8XwZ_FKcRYsg
+ * 
+ */
+
+/**
+ * 本文阿宝哥将按照以下的流程来介绍前端如何进行图片处理，
+ * 然后穿插介绍二进制、Blob、Blob URL、Base64、Data URL、ArrayBuffer、TypedArray、DataView 和图片压缩相关的知识点。
+ */
+
+/**
+ * 讲的很细致 Blob URL、Base64、Data URL这几个理解的很清楚
+ */
