83fa0d61a334960596c4da8df61dac7cb49c409b
chuang.wang <chuang.wang@ubtrobot.com>
2021-12-09 16:04:17 +0800

    del

--- webpack/src/index.js
+++
@@ -1,6 +0,0 @@
-import add from './moduleA.js';
-import multiply from './moduleB.js';
-
-const res = add(1, 2);
-const result = multiply(res, 2);
-console.log(result);
--- webpack/src/moduleB.js
+++
@@ -1,5 +0,0 @@
-import add from './moduleA.js';
-
-export default function multiply(a, b) {
-  return a * add(a, b);
-}
