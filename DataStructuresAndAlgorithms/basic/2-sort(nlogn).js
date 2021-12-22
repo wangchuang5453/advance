4f9ca9abcf3ece3d3b8d75e0b89e063db78ae50f
chuang.wang <chuang.wang@ubtrobot.com>
2021-12-22 17:10:13 +0800

    quick

--- DataStructuresAndAlgorithms/basic/2-sort(nlogn).js
+++ DataStructuresAndAlgorithms/basic/2-sort(nlogn).js
@@ -1,5 +1,5 @@
 const { insertionSortLR } = require('./1-sort(n^2).js');
-
+const { swap } = require('./sortTestHelper.js');
 /**
  * 归并排序 Merge Sort
  * 
@@ -63,7 +63,8 @@ function _merge(arr, l, mid, r) {
 /**
  * merge sort 自底向上
  * 
- * 意义所在？
+ * 意义所在？链表什么blabla
+ * 
  */
 
 function mergeSortBU(arr) { // 比上面的慢一些
@@ -75,7 +76,192 @@ function mergeSortBU(arr) { // 比上面的慢一些
   }
 }
 
+/**
+ * Quick Sort
+ * 快速排序
+ * 20世纪对世界影响最大的算法之一，能以非常快的速度完成排序
+ * 但是即使如此，也是经过了很长时间的改进和优化才被公认为可以被信任的非常优秀的算法
+ */
+
+
+function quickSort(arr) {
+  const n = arr.length;
+  _quickSort(arr, 0, n-1);
+}
+
+function quickSort2(arr) {
+  const n = arr.length;
+  _quickSort2(arr, 0, n-1);
+}
+
+function _quickSort(arr, l, r) {
+  if (l>= r) {
+    return;
+  }
+  const p = _partition(arr, l, r);
+  _quickSort(arr, l, p-1);
+  _quickSort(arr, p+1, r)
+}
+
+function _quickSort2(arr, l, r) {
+  if (r - l <= 15) { // 优化
+    insertionSortLR(arr, l, r);
+    return;
+  }
+  const p = _partition(arr, l, r); // 优化
+  _quickSort2(arr, l, p-1);
+  _quickSort2(arr, p+1, r)
+}
+
+function _partition(arr, l, r) {
+  let v = arr[l];
+  let j = l;
+  for (let i = l + 1; i <= r; i++) {
+    if (arr[i] < v) {
+      swap(arr, j+1, i);
+      j++;
+    }
+  }
+  swap(arr, j, l);
+  return j;
+}
+
+/**
+ * quickSort quickSort2
+ * 这种快排，和归并排序相比，都是将数组拆分成两部分，
+ * 但是归并排序是进行平均等分，层数是logn，现在的快排拆分出的数据很可能是一大一小的，
+ * 拆分出的层数超过logn，最差可能退化为O(n^2)
+ * 数组有序情况下，最左边就是最小的，那就拆分出第一个和剩下的
+ * 这样拆分下去递归，就是层数为n，每一层处理又是n，就是n^2的复杂度
+ * 
+ * 所以优化点就是，尽量选择中间数值的那个是最好的，但是不能快速准确定位这个中间元素
+ * 那就选择使用一个随机数据就可以，此时算出的时间复杂度*期望值*是nlogn，不是每次都是nlogn
+ * 但是这样退化成n^2是不可能的，因为如果每一次递归都选择到最小值的概率是1/n * 1/(n-1)...
+ * 这个概率接近0
+ */
+
+function quickSort3(arr) {
+  const n = arr.length;
+  _quickSort3(arr, 0, n-1);
+}
+
+function _quickSort3(arr, l, r) {
+  if (r - l <= 15) { // 优化
+    insertionSortLR(arr, l, r);
+    return;
+  }
+  const p = _partition2(arr, l, r); // 优化
+  _quickSort3(arr, l, p-1);
+  _quickSort3(arr, p+1, r)
+}
+
+function _partition2(arr, l, r) {
+  // 优化
+  let rIndex = Math.floor(Math.random()*(r-l+1)) + l;
+  swap(arr, l, rIndex);
+
+  let v = arr[l];
+  let j = l;
+  for (let i = l + 1; i <= r; i++) {
+    if (arr[i] < v) {
+      swap(arr, j+1, i);
+      j++;
+    }
+  }
+  swap(arr, j, l);
+  return j;
+}
+
+/**
+ * 如果有大量重复数据，那么等于v的数据会出现非常多，处在v的同一侧，会导致不平衡
+ * 进一步优化，使得相等的数据被分散，近乎平均分配在v的左右两侧
+ * 
+ * 二路快速排序
+ */
+function quickSort4(arr) {
+  const n = arr.length;
+  _quickSort4(arr, 0, n-1);
+}
+
+function _quickSort4(arr, l, r) {
+  if (r - l <= 15) { // 优化
+    insertionSortLR(arr, l, r);
+    return;
+  }
+  const p = _partition4(arr, l, r); // 优化
+  _quickSort4(arr, l, p-1);
+  _quickSort4(arr, p+1, r)
+}
+
+function _partition4(arr, l, r) {
+  let rIndex = Math.floor(Math.random()*(r-l+1)) + l;
+  swap(arr, l, rIndex);
+
+  let v = arr[l];
+  let i = l + 1; let j = r;
+  while(true) {
+    while(i <= r && arr[i] < v) i++;
+    while(j >= l + 1 && arr[j] > v) j--;
+    if (i > j) {
+      break;
+    }
+    swap(arr, i, j); // 换的数据包含和v相等的，解决了相等数据不平衡问题，换完之后往前走看下一个去了
+    i++;
+    j--;
+  }
+  swap(arr, j, l);
+
+  return j;
+}
+
+/**
+ * 三路快速排序
+ * 
+ * 等于v的数据被放在中间，下次排序只需要考虑小于v和大于v的部分，
+ * 省去了对这部分相等数据的处理
+ */
+function quickSort3Way(arr) {
+  const n = arr.length;
+  _quickSort3Way(arr, 0, n-1);
+}
+
+function _quickSort3Way(arr, l, r) {
+  if (r - l <= 15) { // 优化
+    insertionSortLR(arr, l, r);
+    return;
+  }
+
+  let rIndex = Math.floor(Math.random()*(r-l+1)) + l;
+  swap(arr, l, rIndex);
+  let v = arr[l];
+  let lt = l;
+  let gt = r + 1;
+  let i = l + 1;
+  while(i < gt) { // i处在gt和相等的数据之间
+    if (arr[i] < v) {
+      swap(arr, lt + 1, i);
+      lt ++;
+      i ++;
+    } else if (arr[i] > v) {
+      swap(arr, gt - 1, i);
+      gt --;
+    } else {
+      i++;
+    }
+  }
+  swap(arr, l, lt);
+
+  _quickSort3Way(arr, l, lt -1);
+  _quickSort3Way(arr, gt, r);
+}
+
+
 module.exports = {
   mergeSort,
   mergeSortBU,
+  quickSort,
+  quickSort2,
+  quickSort3,
+  quickSort4,
+  quickSort3Way,
 }
--- DataStructuresAndAlgorithms/basic/test.js
+++ DataStructuresAndAlgorithms/basic/test.js
@@ -6,20 +6,48 @@ const {
   shellSort,
 } = require('./1-sort(n^2).js');
 
-const { mergeSort, mergeSortBU } = require('./2-sort(nlogn).js');
+const { mergeSort, mergeSortBU, quickSort, quickSort2, quickSort3, quickSort4, quickSort3Way } = require('./2-sort(nlogn).js');
 
-const bubbleArr = generateRandomArray(50000, 0, 100000);
-// const bubbleArr = generateNearlyOrderedArray(50000, 100)
+// const bubbleArr = generateRandomArray(1000000, 0, 10000000);
+// const bubbleArr = generateNearlyOrderedArray(1000000, 100)
 // const bubbleArr = generateNearlyOrderedArray(10000, 100).reverse();
 // const bubbleArr = generateNearlyOrderedArray(10000, 0);
-
+// 重复数组
+const bubbleArr = generateRandomArray(1000000, 0, 10);
 
 // testSort(selectionSort, bubbleArr.concat([]))
-testSort(insertionSort3, bubbleArr.concat([]));
+// testSort(insertionSort3, bubbleArr.concat([]));
 // testSort(bubbleSort, bubbleArr.concat([]));
 // testSort(shellSort, bubbleArr.concat([]));
 
 
 testSort(mergeSort, bubbleArr.concat([]));
-testSort(mergeSortBU, bubbleArr.concat([]));
+// testSort(mergeSortBU, bubbleArr.concat([]));
+// testSort(quickSort, bubbleArr.concat([]));
+
+/**
+ * *有序数据* 排序现在的快排（quickSort2）超级慢
+ */
+// testSort(quickSort2, bubbleArr.concat([]));
+
+
+/**
+ * 进一步优化后处理有序数组
+ */
+// testSort(quickSort3, bubbleArr.concat([]));
+
+/**
+ * 优化，处理大量重复数据的数组
+ * 2路快排
+ */
+testSort(quickSort4, bubbleArr.concat([]));
+
+ /**
+  * 优化，处理大量重复数据的数组最快，无序数组和二路差不多，顺序数组二路快但是三路也可以保证，归并都是最慢
+  * 各有优劣
+  * 
+  * 3路快排
+  */
+testSort(quickSort3Way, bubbleArr.concat([]));
+
 
