dba11f0470c4854c9ced067f55934cc9356353f7
wangchuang <wangch5453@163.com>
2022-01-08 11:51:04 +0800

    heap sort

--- DataStructuresAndAlgorithms/basic/sort/3-heapSort.js
+++ DataStructuresAndAlgorithms/basic/sort/3-heapSort.js
@@ -19,8 +19,11 @@ class MaxHeap {
     }
   }
 
-  _shiftDown(k) {
-    while(2*k <= this.count) {
+  // 执行一次只能为某个数据找到正确的位置，不能保证二叉树符合堆的性质
+  // 见下方heapify，使用_shiftDown得到二叉堆
+  // extractMax是在已经是二叉堆的基础上执行了一次，得到最大值
+  _shiftDown(k) { 
+    while(2*k <= this.count) { // 看子元素是否越界
       let j = 2*k;
       if (j+1 <= this.count && this._data[j+1] > this._data[j]) {
         j++;
@@ -72,7 +75,12 @@ class MaxHeap {
 }
 
 /**
- * 
+ * Heapify
+ * 把一个数组构建成堆的更好的方法
+ * 完全二叉树第一个不是叶子节点的元素是最后一个节点/2
+ * 所有叶子节点（没有子元素）都是最大堆了
+ * heapity.png图
+ * 从第一个不是叶子节点的元素向前进行shiftDown
  */
 class MaxHeap2 {
   constructor(arr, n) {
@@ -95,8 +103,9 @@ class MaxHeap2 {
   }
 
   _shiftDown(k) {
-    while(2*k <= this.count) {
+    while(2*k <= this.count) { // 完全二叉树，2k可以指向最后一个子节点，所以判断2k
       let j = 2*k;
+      // j+1是j非最后一个子节点情况
       if (j+1 <= this.count && this._data[j+1] > this._data[j]) {
         j++;
       }
@@ -171,14 +180,14 @@ function heapSort1(arr) {
 }
 
 /**
- * Heapify
+ * Heapify的方式
  * 
  */
 function heapSort2(arr) {
   const n = arr.length;
   const maxHeap = new MaxHeap2(arr, n);
   for (let i = n-1; i >= 0; i--) {
-    arr[i] = maxHeap.extractMax();
+    arr[i] = maxHeap.extractMax(); // 最大值放最后，从小到大
   }
 }
 
@@ -186,15 +195,21 @@ function heapSort2(arr) {
  * 优化的堆排序
  * (Heap Sort)
  * 原地堆排序 从索引0开始
+ * 不采用插入到堆中的操作，直接操作这个数组
+ * 
  */
 
 function heapSort3(arr, n) {
   // heapify
-  // 从0开始计算
+  // 注意，此时我们的堆是从0开始索引的
+  // 从(最后一个元素的索引-1)/2开始
+  // 最后一个元素的索引 = n-1
   for (let i = Math.floor((n-1-1)/2); i >= 0; i--) {
     _shiftDown3(arr, n, i);
   }
-
+  // 经过上一步的heapify，此时第一个数据就是最大值
+  // 最大值和最后一个数据交换
+  // 然后继续shiftdown，得到从小到大的顺序
   for (let i = n-1; i > 0 ; i--) {
     swap(arr, 0, i);
     _shiftDown3(arr, i, 0);
@@ -202,7 +217,7 @@ function heapSort3(arr, n) {
 }
 
 function _shiftDown3(arr, n, k) {
-  while(2*k + 1 < n) {
+  while(2*k + 1 < n) { // 完全二叉树（都在左侧）最后一个子节点
     let j = 2*k + 1;
     if (j+1 < n && arr[j+1] > arr[j]) {
       j++;
