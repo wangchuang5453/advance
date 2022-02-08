97f75209c8ef26eb7243bfa71a21faf198e6db12
wangchuang <wangch5453@163.com>
2022-02-08 21:46:52 +0800

    eventloop question

---
+++ jsBasicKnowledge/eventLoop/question.js
@@ -0,0 +1,37 @@
+async function async1() {
+  console.log('async1 start');
+  await async2();
+  console.log('async1 end');
+}
+
+async function async2() {
+  console.log('async2'); // 立即执行 .then进入微任务队列
+}
+
+console.log('script start');
+
+setTimeout(() => {
+  console.log('setTimeout');
+}, 0);
+
+async1();
+
+new Promise((resolve) => {
+  console.log('promise1'); // 立即执行 .then进入微任务队列
+  resolve();
+}).then(() => {
+  console.log('promise2');
+});
+
+console.log('script end');
+
+// script start 
+// async1 start
+// async2
+// promise1
+// script end
+// async1 end
+// promise2
+// setTimeout
+
+
