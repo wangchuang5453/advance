2c8a7cbeca4dd0b83bead1d4c79ffc4b4a486b10
chuang.wang <chuang.wang@ubtrobot.com>
2022-03-11 19:29:22 +0800

    css-loader

--- engineering/webpack/loaders/postcss-loader/index.md
+++ engineering/webpack/loaders/postcss-loader/index.md
@@ -2,5 +2,4 @@
 
 Postcss 本身是一个功能比较单一的工具。它提供了一种方式用 JavaScript 代码来处理 CSS。它负责把 CSS 代码解析成抽象语法树结构（Abstract Syntax Tree，AST），再交由插件来进行处理。插件基于CSS 代码的 AST 所能进行的操作是多种多样的，比如可以支持变量和混入（mixin），增加浏览器相关的声明前缀，或是把使用将来的 CSS 规范的样式规则转译（transpile）成当前的 CSS 规范支持的格式。从这个角度来说，PostCSS 的强大之处在于其不断发展的插件体系。
 
-# css-loader
 
---
+++ engineering/webpack/test/src/css-loader.js
@@ -0,0 +1,18 @@
+import style from './css/1.css';
+
+console.log(style);
+
+// __webpack_require__.r(__webpack_exports__);
+// __webpack_require__.d(__webpack_exports__, {
+//     "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+// });
+// var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__( "./node_modules/css-loader/dist/runtime/noSourceMaps.js");
+// var _node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default = __webpack_require__.n(_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0__);
+// var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__("./node_modules/css-loader/dist/runtime/api.js");
+// var _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default = __webpack_require__.n(_node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1__);
+// // Imports
+// var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_noSourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));
+// // Module
+// ___CSS_LOADER_EXPORT___.push([module.id, "html,\\nbody {\\n  background-color: #ccc;\\n  height: 100vh;\\n}\\n\\nh1 {\\n  font-size: 100px;\\n}\\n\\n\\n"]);
+// // Exports
+// const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
